93ffcb23ee2ecab8c1fff96e32e3a6688a07fd0c
Huanghe <xs28@rice.edu>
2024-08-08 13:34:31 -0500

    Try not using boilerplate

--- doxypypy/doxypypy.py
+++ doxypypy/doxypypy.py
@@ -845,10 +845,7 @@ class AstWalker(NodeVisitor):
 
     def _group_special_decorator(self, indentStr,header, start_lineno: int, end_lineno: int):
         _decorator = self.lines[start_lineno]
-        line = (f"{indentStr}## @name {header}{linesep}"
-                f"{indentStr}###@{{{linesep}"
-                f"{linesep}"
-                f"{indentStr}###@}}{linesep}{linesep}")
+        line = ""
         # boilerplate to make rendering correct
         line += (f"{indentStr}## @name {header}{linesep}"
                  f"{indentStr}###@{{{linesep}{_decorator}")
